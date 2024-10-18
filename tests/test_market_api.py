d4f032fb241c9767ad5915435046655d0b5d4cc2
May Bär <m.baer@iasonconsulting.com>
2024-10-18 12:00:54 +0200

    Make market_values fetch default, show embedding

--- tests/test_market_api.py
+++ tests/test_market_api.py
@@ -34,6 +34,8 @@ class TestMarketApi:
 
         # Assert
         assert meta_data.id == 22118
+        assert meta_data.get_wiki_link() == "https://tibia.fandom.com/wiki/22118"
+        assert meta_data.get_image_link() == "https://www.tibiamarket.top/sprites/22118.gif"
 
     async def test_get_meta_data_invalid_identifier_throws(self):
         """Test the get_meta_data method with an invalid identifier."""
