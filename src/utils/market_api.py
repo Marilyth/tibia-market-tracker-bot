7974cb021e5d84d8270a070b48fa9112f60b9c18
May Bär <m.baer@iasonconsulting.com>
2024-10-18 15:06:49 +0200

    Add market board to api wrapper

--- src/utils/market_api.py
+++ src/utils/market_api.py
@@ -25,8 +25,9 @@ class MarketApi:
 
         self.world_data: CacheableData[Dict[str, WorldData]] = CacheableData(self._load_world_data, invalidate_after_seconds=60)
         self.meta_data: CacheableData[Dict[int, ItemMetaData]] = CacheableData(self._load_meta_data, invalidate_after_seconds=3600)
-        self.market_values_cache: Dict[str, CacheableData[int, MarketValues]] = {}
-        self.history_cache: Dict[str, CacheableData[int, List[MarketValues]]] = {}
+        self.market_values_cache: Dict[str, CacheableData[MarketValues]] = {}
+        self.history_cache: Dict[str, CacheableData[List[MarketValues]]] = {}
+        self.market_board_cache: Dict[str, CacheableData[MarketBoard]] = {}
 
     @staticmethod
     def normalize_identifier(identifier: str) -> str:
@@ -100,6 +101,27 @@ class MarketApi:
 
         return history
 
+    async def get_market_board(self, server: str, identifier: str) -> MarketBoard:
+        """Get the market board of an item by it's identifier.
+
+        Args:
+            server (str): The name of the Tibia server.
+            identifier (str): The identifier of the item.
+
+        Returns:
+            MarketBoard: The market board of the item.
+        """
+        item_id: int = await self.identifier_to_item_id(identifier)
+        key = f"{server}_{item_id}"
+
+        if key not in self.market_board_cache:
+            self.market_board_cache[key] = CacheableData(lambda: self._load_market_board(server, item_id), invalidate_after_seconds=300, delete_after_interval=True)
+
+        last_world_update = (await self.world_data.get_async())[server].last_update.timestamp()
+        market_board = await self.market_board_cache[key].get_async(last_world_update)
+
+        return market_board
+
     async def get_meta_data(self, identifier: str) -> ItemMetaData:
         """Get the meta data of an item by it's identifier.
 
@@ -144,6 +166,20 @@ class MarketApi:
         response = await self._send_request("item_history", server=server, item_id=item_id)
 
         return [MarketValues(**item) for item in response]
+
+    async def _load_market_board(self, server: str, item_id: int) -> MarketBoard:
+        """Loads and caches the market board of an item in a Tibia server.
+
+        Args:
+            server (str): The name of the Tibia server.
+            item_id (int): The id of the item.
+
+        Returns:
+            MarketBoard: The market board of an item in a Tibia server.
+        """
+        response = await self._send_request("market_board", server=server, item_id=item_id)
+
+        return MarketBoard(**response)
 
     async def _load_world_data(self) -> Dict[str, WorldData]:
         """Loads and caches the world data of all Tibia servers.
